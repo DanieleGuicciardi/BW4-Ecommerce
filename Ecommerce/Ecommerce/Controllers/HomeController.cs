e57bea456d84eb0039d390724ac0ea18a9cb6b68
Manuel Palmieri <manuelpalm2002@gmail.com>
2025-03-03 16:51:40 +0100

    creata get per admin page e view

--- Ecommerce/Ecommerce/Controllers/HomeController.cs
+++ Ecommerce/Ecommerce/Controllers/HomeController.cs
@@ -1,3 +1,4 @@
+using Microsoft.Data.SqlClient;
 using System.Diagnostics;
 using Ecommerce.Models;
 using Microsoft.AspNetCore.Mvc;
@@ -14,7 +15,7 @@ namespace Ecommerce.Controllers
         {
             var configuration = new ConfigurationBuilder()
                 .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile("appsettings.json", false, true) 
+                .AddJsonFile("appsettings.json", false, true)
                 .Build();
 
             _connectionString = configuration.GetConnectionString("DefaultConnection");
@@ -30,7 +31,44 @@ namespace Ecommerce.Controllers
         {
             return View();
         }
+        public async Task<IActionResult> AdminPage()
+        {
+            var productsList = new AdminProductsViewModel()
+            {
+                AdminProducts = new List<AdminProduct>()
+            };
 
-        
+            await using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                //se non apro la connessione, non mi connetto al db e non posso eseguire query
+                await connection.OpenAsync();
+                string query = "SELECT PRODUCTS.Id, PRODUCTS.Name, Price, DescriptionShort, IdCategory, CATEGORIES.Name FROM PRODUCTS INNER JOIN CATEGORIES ON IdCategory = CATEGORIES.Id ORDER BY IdCategory;";
+
+                await using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    await using (SqlDataReader reader = await command.ExecuteReaderAsync())
+                    {
+                        while (await reader.ReadAsync())
+                        {
+                            productsList.AdminProducts.Add(
+                                new AdminProduct()
+                                {
+                                    Id = reader.GetGuid(0),
+                                    Name = reader.GetString(1),
+                                    Price = reader.GetDecimal(2),
+                                    DescriptionShort = reader.GetString(3),
+                                    IdCategory = reader.GetInt32(4),
+                                    CategoryName = reader.GetString(5),
+                                }
+                            );
+                        }
+                    }
+                }
+
+                return View(productsList);
+            }
+
+
+        }
     }
 }
---
+++ Ecommerce/Ecommerce/Models/AdminProduct.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.SqlTypes;
+
+namespace Ecommerce.Models
+{
+    public class AdminProduct
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public SqlMoney Price { get; set; }
+        public string DescriptionShort { get; set; }
+        public int IdCategory { get; set; }
+        public string CategoryName { get; set; }
+    }
+}
