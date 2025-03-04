e584b93ed6ebe834cf90825d271c07c629d0094c
Manuel Palmieri <manuelpalm2002@gmail.com>
2025-03-04 09:41:17 +0100

    aggiunti display e required ai form

--- Ecommerce/Ecommerce/Models/EditProduct.cs
+++ Ecommerce/Ecommerce/Models/EditProduct.cs
@@ -1,18 +1,32 @@
-﻿using System.Data.SqlTypes;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Data.SqlTypes;
 
 namespace Ecommerce.Models
 {
     public class EditProduct
     {
         public Guid Id { get; set; }
+        [Display(Name = "Nome Prodotto")]
+        [Required(ErrorMessage = "il nome è obbligatorio")]
         public string? Name { get; set; }
+        [Display(Name = "Prezzo")]
+        [Required(ErrorMessage = "il prezzo è obbligatorio")]
         public decimal Price { get; set; }
+        [Display(Name = "Descrizione")]
+        [Required(ErrorMessage = "la descrizione è obbligatoria")]
         public string? Description { get; set; }
+        [Display(Name = "Descrizione corta")]
+        [Required(ErrorMessage = "la descrizione corta è obbligatoria")]
         public string? DescriptionShort { get; set; }
+        [Display(Name = "Immagine Principale")]
+        [Required(ErrorMessage = "l'immagine principale è obbligatoria")]
         public string? Img { get; set; }
+        [Display(Name = "Immagine 2")]
         public string? Img2 { get; set; }
+        [Display(Name = "Immagine 3")]
         public string? Img3 { get; set; }
-
+        [Display(Name = "Categoria")]
+        [Required(ErrorMessage = "la categoria è obbligatoria")]
         public int IdCategory { get; set; }
     }
 }
